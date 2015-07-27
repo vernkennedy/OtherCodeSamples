d4066248fa28c1396322e50f714951f474a9d934
vernkennedy <vzidana14@wooster.edu>
2015-07-27 16:31:57 -0700

    3

--- javascripts/OtherCodeSamples.js
+++ javascripts/OtherCodeSamples.js
@@ -4,40 +4,70 @@ var PHPProjectSPortfolio =
   "PHPProjects": [
     
 	 {
-		"id":3,
+		"id":1,
        "project_name": "ClientSide Google Map",
       "owner": {       
         "avatar_url": "map.jpg", 
       },           
-      "description": "https://github.com/vernkennedy/ClientSideGoogleMap/blob/gh-pages/description.txt",      
+      "description": "https://github.com/vernkennedy/ClientSideGoogleMap/blob/gh-pages/description.txt", 
+	  "summary": "A Rule Based Expert System / web portal for academic planning and exploration of student opportunities at The College of Wooster",
       "code_url": "https://github.com/vernkennedy/ClientSideGoogleMap/tree/gh-pages",
 	  "demo_url": "http://vernkennedy.github.io/ClientSideGoogleMap/",
       "frontend_infor": "Html5,CSS3,JavaScript,Html5 Drag and Drop API",      
-      "backend_infor": "Php5,SQL,MySQL"  
+      "backend_infor": "Php5,SQL,MySQL",
+	  "demotype":"Live Demo"
     },
   
     {
-		"id":4,
+		"id":2,
       "project_name": "Drupal Module",
       "owner": {       
         "avatar_url": "drupal.jpg", 
       },           
-      "description": "https://github.com/vernkennedy/DrupalModule/blob/master/readme.txt",      
+      "description": "https://github.com/vernkennedy/DrupalModule/blob/master/readme.txt",
+	  "summary": "A Rule Based Expert System / web portal for academic planning and exploration of student opportunities at The College of Wooster",
       "code_url": "https://github.com/vernkennedy/DrupalModule", 
 	   "demo_url": "https://www.youtube.com/watch?v=dxbr7o5pThM&feature=youtu.be",
       "frontend_infor": "Html5,CSS3",      
-      "backend_infor": "Php5,SQL,MySQL"  
-    }
+      "backend_infor": "Php5,SQL,MySQL",
+	  "demotype":"Video Demo"
+    },
+	
+	{
+		"id":3,
+      "project_name": "JSON Portifolio",
+      "owner": {       
+        "avatar_url": "drupal.jpg", 
+      },           
+      "description": "https://github.com/vernkennedy/DrupalModule/blob/master/readme.txt",
+	  "summary": "A Rule Based Expert System / web portal for academic planning and exploration of student opportunities at The College of Wooster",
+      "code_url": "https://github.com/vernkennedy/DrupalModule", 
+	   "demo_url": "https://www.youtube.com/watch?v=dxbr7o5pThM&feature=youtu.be",
+      "frontend_infor": "Html5,CSS3",      
+      "backend_infor": "Php5,SQL,MySQL",
+	  "demotype":"Live Demo"
+    },
+	
+	
   ]
 }
 
 //-----------------------------------------------------------------------insert data above
 
-text = "";
+content = "";
 var i;
 for (i = 0; i < PHPProjectSPortfolio.PHPProjects.length; i++) {
-    text +="<li><img src="+PHPProjectSPortfolio.PHPProjects[i].owner.avatar_url+"><br><br><h3 align=center>"+ PHPProjectSPortfolio.PHPProjects[i].project_name + "</h3><a href="+PHPProjectSPortfolio.PHPProjects[i].description+ ">Description</a> <a href="+PHPProjectSPortfolio.PHPProjects[i].demo_url+ ">Demo</a> <a href="+PHPProjectSPortfolio.PHPProjects[i].code_url + ">Code</a><br><br><br><br><br></li>";
+    content +="<li>"
+				+"<img src="+PHPProjectSPortfolio.PHPProjects[i].owner.avatar_url+">"
+				+"<h3 align=center>"+ PHPProjectSPortfolio.PHPProjects[i].project_name +"</h3>"
+				+"<a href="+PHPProjectSPortfolio.PHPProjects[i].description+">Full Description</a>"
+				+" <a href="+PHPProjectSPortfolio.PHPProjects[i].demo_url+">"+PHPProjectSPortfolio.PHPProjects[i].demotype+"</a>"
+				+" <a href="+PHPProjectSPortfolio.PHPProjects[i].code_url + ">Code</a><br><br>"
+				+"<b>Summary</b><br>"+PHPProjectSPortfolio.PHPProjects[i].summary + "<br><br>"
+				+"<b>Front-end Technologies used:</b><br>"+PHPProjectSPortfolio.PHPProjects[i].frontend_infor +"<br><br>"
+				+"<b>Middle Tier and Backend Technologies used:</b><br>"+ PHPProjectSPortfolio.PHPProjects[i].backend_infor 
+			+"</li>";
 }
 
 
-document.getElementById("demo").innerHTML = "<ol>"+text+"</ol>";
+document.getElementById("demo").innerHTML = "<ol>"+content+"</ol>";
