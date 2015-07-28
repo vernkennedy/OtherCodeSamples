ee158016da524a620867160e534164f85e7dd14b
vernkennedy <vzidana14@wooster.edu>
2015-07-28 08:31:44 -0700

    6

--- javascripts/OtherCodeSamples.js
+++ javascripts/OtherCodeSamples.js
@@ -1,5 +1,5 @@
 var app = angular.module('myApp', []);
 app.controller('customersCtrl', function($scope, $http) {
-  $http.get("https://raw.githubusercontent.com/vernkennedy/JAVA-J2EEProjects/gh-pages/javascripts/J2EEProjects.json")
+  $http.get("https://raw.githubusercontent.com/vernkennedy/OtherCodeSamples/gh-pages/javascripts/OtherCodeSamples.json")
   .success(function (response) {$scope.names = response.records;});
 });
