c5775147841e2b5a5354ea1a72f05f22ad450af1
vernkennedy <vzidana14@wooster.edu>
2015-10-06 07:07:19 -0700

    89

--- javascripts/OtherCodeSamples.js
+++ javascripts/OtherCodeSamples.js
@@ -3,3 +3,4 @@ app.controller('customersCtrl', function($scope, $http) {
   $http.get("https://raw.githubusercontent.com/vernkennedy/OtherCodeSamples/gh-pages/javascripts/OtherCodeSamples.json")
   .success(function (response) {$scope.names = response.records;});
 });
+ 
